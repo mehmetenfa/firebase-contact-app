775f30b354fad7c2eda23723eff4c14d30619b3e
qxan <mehmet.enfa@gmail.com>
2022-11-14 21:19:37 +0300

    form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
 import Contacts from "./components/contacts/Contacts";
 import FormComponent from "./components/form/FormComponent";
+import {useState} from "react"
 
+const initialValues={
+  username:"",
+  phoneNumber:"",
+  gender:""
+}
 function App() {
+  const [info, setInfo] = useState(initialValues)
+  
+  const handleSubmit=(e)=>{
+e.preventDefault();
+  }
+
+
   return (
     <div className="App">
-      <FormComponent />
+      <FormComponent info={info} setInfo={setInfo} handleSubmit={handleSubmit} />
       <Contacts />
     </div>
   );
